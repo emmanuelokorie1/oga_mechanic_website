cd7dda43b804429c95e92dd49596642dbd3004da
Mac <bookpro@Emmzzys-Macbook-Pro.local>
2026-02-02 01:04:17 +0100

    feat: Comment out the ChatWidget component in the main layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,7 +74,7 @@ export default function RootLayout({
             <Header />
             {children}
             {/* <TawkTo /> */}
-            <ChatWidget />
+            {/* <ChatWidget /> */}
             <Toaster position="top-center" reverseOrder={false} />
             <BackToTop />
           </SmoothScroller>
